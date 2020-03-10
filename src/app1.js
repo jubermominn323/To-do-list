cbd8a654760f61b42611840bcf74fe6f7809ecab
jubermomin323 <jubermomin323@gmail.com>
2020-03-10 21:44:20 +0530

    The second commit

--- src/app1.js
+++ src/app1.js
@@ -4,35 +4,35 @@ const fs = require('fs');
 
 
 app.get('/' , function(req , res) {
-    fs.readFile("html/index.html" , function(err, data) {
+    fs.readFile("src/html/index.html" , function(err, data) {
         res.send(data.toString());
     });
     
 })
 
 app.get('/api/todos', function(req, res) {
-    fs.readFile("temp.json", function(err, data) {
+    fs.readFile("src/temp.json", function(err, data) {
         res.send(data.toString())
         
     })
 });
 
-app.get('/api/todos/add', function(req,resp){
+app.get('/api/todos/add', function(req,res){
     console.log(req.query);
     if(typeof(req.query.todoname) != undefined && req.query.todoname != "") {
         var todoName = req.query.todoname;
         console.log("There is a valid new todo data")
         console.log(todoName);
-        fs.readFile("temp.json", function(err,todosData) {
+        fs.readFile("src/temp.json", function(err,todosData) {
             var todoListData = JSON.parse(todosData);
             console.log(todoListData);
             todoListData.data.push({"title":todoName,"checked":false});
-            fs.writeFile("temp.json",JSON.stringify(todoListData), function(err,data) {
-                resp.send("Saved new todo data "+todoName) 
+            fs.writeFile("src/temp.json",JSON.stringify(todoListData), function(err,data) {
+                res.send("Saved new todo data "+todoName) 
             })
         });
     } else {
-        resp.send("No valid new todo data")
+        res.send("No valid new todo data")
     }
 })
 app.get('*', function(req, res) {
