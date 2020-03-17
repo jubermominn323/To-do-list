8d5016377fa6d20b212a8c7d206691bfa9ba0856
jubermomin323 <jubermomin323@gmail.com>
2020-03-18 00:56:20 +0530

    third commit

--- src/app1.js
+++ src/app1.js
@@ -2,43 +2,81 @@ const express = require('express');
 const app = express();
 const fs = require('fs');
 
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient
+const ObjectId = require('mongodb').ObjectID;
+const url = "mongodb://127.0.0.1:27017";
+const databaseName = "todo-db";
 
+MongoClient.connect(url, {useUnifiedTopology:true}, function(err, client) {
+    if(err){ console.log("There is some error")
+    return
+    }
+    else
+     console.log("Connection Success")
+
+    
 app.get('/' , function(req , res) {
     fs.readFile("src/html/index.html" , function(err, data) {
         res.send(data.toString());
     });
-    
 })
 
 app.get('/api/todos', function(req, res) {
-    fs.readFile("src/temp.json", function(err, data) {
-        res.send(data.toString())
+    db.collection("user").find({}).toArray(function(err, data) {
+        res.send(data)
+    })
+})
+
+db = client.db(databaseName)
+
+app.get('/update', function(req,res) {
+    
+    //var hex = /[0-9A-Fa-f]{6}/g;
+    //var id = (hex.test(id))? ObjectId(id) : id;
+    var id = req.query.id;
+    var val = req.query.updateVal
+    console.log(val);
+    db.collection("user").updateOne({"task":"val"},{$set:{"checked":true}}, function(err, res) {
+        if(err)
+        console.log(err)
+        else("updated successfully")
+    })
+})
+
+
+app.get('/delete', function(req, res) {
+    var delt = req.query.del
+  //  var id = req.query.id
+    db.collection("user").deleteOne({"task":delt}, function(err, res) {
+        if(err)
+        console.log(err)
         
     })
-});
+    res.send("Success")
+
+})
+
 
 app.get('/api/todos/add', function(req,res){
-    console.log(req.query);
-    if(typeof(req.query.todoname) != undefined && req.query.todoname != "") {
-        var todoName = req.query.todoname;
-        console.log("There is a valid new todo data")
-        console.log(todoName);
-        fs.readFile("src/temp.json", function(err,todosData) {
-            var todoListData = JSON.parse(todosData);
-            console.log(todoListData);
-            todoListData.data.push({"title":todoName,"checked":false});
-            fs.writeFile("src/temp.json",JSON.stringify(todoListData), function(err,data) {
-                res.send("Saved new todo data "+todoName) 
-            })
-        });
-    } else {
-        res.send("No valid new todo data")
-    }
-})
-app.get('*', function(req, res) {
-    res.send("<h1>Invalid Page</h1>");
-});
 
+    db.collection("user").insertOne({
+        "task":`${req.query.todoname}`,
+        "checked":false
+    })
+
+    db.collection("user").find({"task":`${req.query.todoname}`}).toArray(function(err, data) {
+        
+        res.send("Data saved Successfully");
+            
+    })
+    
+})
+    app.get('*', function(req, res) {
+        res.send("<h1>Invalid Page</h1>");
+    });
+
+})
 
 
 app.listen(process.env.PORT || 2020, function() {
